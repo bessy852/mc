c9a2a2afa92e6ad57c6d414cea685f6a78e1235b
bessy852 <joren_verbesselt_95@msn.com>
2024-05-20 18:23:17 +0200

    startup file

--- mc.sh
+++ mc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /opt/mc && java -Xms4096M -Xmx4096M -jar /opt/mc/forge-1.12.2-14.23.5.2860-universal.jar
+cd /opt/mc && java -Xms4096M -Xmx4096M -jar /opt/mc/forge-1.12.2-14.23.5.2860.jar
